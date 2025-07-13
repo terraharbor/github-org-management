e86dd1833d69c976a337d31282b3926bc24179ca
Gonçalo Heleno <goncalo.carvalheiroheleno@heig-vd.ch>
2025-07-13 11:09:38 +0200

    ci: unset variables GITHUB_OWNER and GITHUB_ORGANIZATION

--- terraform.tf
+++ terraform.tf
@@ -20,8 +20,8 @@ terraform {
 provider "github" {
   owner = var.github_organization
   app_auth {
-    # id              = var.github_app_id              # or `GITHUB_APP_ID`
-    # installation_id = var.github_app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
-    # pem_file        = var.github_app_pem_file        # or `GITHUB_APP_PEM_FILE`
+    id              = var.github_app_id              # or `GITHUB_APP_ID`
+    installation_id = var.github_app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
+    pem_file        = var.github_app_pem_file        # or `GITHUB_APP_PEM_FILE`
   }
 }
