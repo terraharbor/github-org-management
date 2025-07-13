99d8c131111571e8f8f9b5d651dac2cfff328eb3
Gonçalo Heleno <goncalo.carvalheiroheleno@heig-vd.ch>
2025-07-13 10:58:43 +0200

    ci: try using the env vars exclusively

--- variables.tf
+++ variables.tf
@@ -5,32 +5,32 @@ variable "github_organization" {
   default     = "terraharbor"
 }
 
-variable "github_app_id" {
-  description = "ID of the GitHub application used for authentication of the GitHub Terraform provider."
-  type        = string
-  nullable    = false
-  ephemeral   = true
-}
+# variable "github_app_id" {
+#   description = "ID of the GitHub application used for authentication of the GitHub Terraform provider."
+#   type        = string
+#   nullable    = false
+#   ephemeral   = true
+# }
 
-variable "github_app_installation_id" {
-  description = "Installation ID of the GitHub application used for authentication of the GitHub Terraform provider."
-  type        = string
-  nullable    = false
-  ephemeral   = true
-}
+# variable "github_app_installation_id" {
+#   description = "Installation ID of the GitHub application used for authentication of the GitHub Terraform provider."
+#   type        = string
+#   nullable    = false
+#   ephemeral   = true
+# }
 
-variable "github_app_pem_file" {
-  description = <<-EOT
-    Content of the PEM file of the GitHub application used for authentication of the GitHub Terraform provider.
+# variable "github_app_pem_file" {
+#   description = <<-EOT
+#     Content of the PEM file of the GitHub application used for authentication of the GitHub Terraform provider.
 
-    Needs to be in the format:
+#     Needs to be in the format:
 
-    -----BEGIN RSA PRIVATE KEY-----
-    ...
-    -----END RSA PRIVATE KEY-----
-  EOT
-  type        = string
-  nullable    = false
-  sensitive   = true
-  ephemeral   = true
-}
+#     -----BEGIN RSA PRIVATE KEY-----
+#     ...
+#     -----END RSA PRIVATE KEY-----
+#   EOT
+#   type        = string
+#   nullable    = false
+#   sensitive   = true
+#   ephemeral   = true
+# }
